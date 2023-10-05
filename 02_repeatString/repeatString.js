7d6c40e5b85c002eae4d87caeae26853c70bd28a
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-05 22:31:17 +0500

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times > -1) {
+    let sentence = "";
+    for (i = 0; i < times; i++) {
+      sentence = sentence + word;
+    }
+    return sentence;
+  } else return "ERROR";
 };
 
 // Do not edit below this line
