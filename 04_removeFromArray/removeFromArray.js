2b44c1b01aab324e5e9833f7dceeadb26c27b81f
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-05 23:23:31 +0500

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...args) {
+  for (arg of args) {
+    position = array.indexOf(arg);
+    if (position != -1) {
+      newArray = array.splice(position, 1);
+    }
+  }
+  return array;
+}
 
-};
+removeFromArray([1, 2, 3], 2, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
