7d56554778cee5c7d7c2f293da4ddd532f2b1363
Huzefa <huzefazaheer1+git@gmail.com>
2023-10-05 22:39:00 +0500

    Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let output = "";
+  for (i = input.length - 1; i > -1; i--) {
+    output = output + input[i];
+  }
+  return output.toString();
 };
 
 // Do not edit below this line
